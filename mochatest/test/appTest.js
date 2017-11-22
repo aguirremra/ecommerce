4eb33529a71b1f0dc855b404213f898b325a3e45
yaw owusu <asirifiyaw@gmail.com>
2017-11-21 22:19:50 -0800

    Expanded  unit test to check for all modules, updated test path and dev-dependencies, renamed mochatest folder to test

--- mochatest/test/appTest.js
+++
@@ -1,8 +0,0 @@
-const assert = require("chai").assert;
-const app = require("../app");
-
-describe("App", function(){
-	it("app should return ready!", function(){
-		assert.equal(app(), "ready!");
-	});
-});
---
+++ test/ReadMe.txt
@@ -0,0 +1,8 @@
+## Running Tests
+
+Dev Dependencies: Mocha
+[Mocha](https://mochajs.org) based test.
+
+
+how to run test:
+run `npm test`
